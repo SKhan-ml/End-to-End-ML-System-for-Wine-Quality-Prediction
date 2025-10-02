4ae796571aead1f19fd6675555a5f2d444662531
khan_shahadath <khanshahadth123@gmail.com>
2025-10-02 18:24:54 +0900

    folder structure added

---
+++ src/wineprediction/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,17 @@
+from wineprediction.config.configuration import ConfigurationManager
+from wineprediction.components.model_trainer import ModelTrainer
+from wineprediction import logger
+
+
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
---
+++ src/wineprediction/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,16 @@
+
+from wineprediction.config.configuration import ConfigurationManager
+from wineprediction.components.model_evaluation import ModelEvaluation
+from wineprediction import logger
+
+STAGE_NAME = "Model evaluation stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
